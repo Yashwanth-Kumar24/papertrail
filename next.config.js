eb8e7e7392e21d775323d533463ce7357a49ac80
Yashwanth-Kumar24 <yashwanth.jannaikode@gmail.com>
2026-05-31 00:45:54 -0400

    fix: remove webpack config for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['peyeyffinmtwcocafkwd.supabase.co'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false }
-    return config
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+    ],
   },
 }
 
